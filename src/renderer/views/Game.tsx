1c282cee8d309bbbee71bbcbe3877917a10697f4
alexpargon <alexpargon@gmail.com>
2025-09-25 23:27:08 +0200

    fix: changed ts config to properly recognize main paths

--- src/renderer/views/Game.tsx
+++ src/renderer/views/Game.tsx
@@ -6,7 +6,7 @@ import GameNav from "@Components/game/GameNav";
 import Render from "@Components/canvas/Render";
 import System from "@Components/canvas/System";
 import starCatalog from "@Assets/data/nearest.json";
-import generateStarSystem from "@Main/logic/aigenerator";
+import generateStarSystem from "@Renderer/logic/aigenerator";
 
 const Game = () => {
   const [mode, setMode] = useState<"starMap" | "starView">("starMap");
